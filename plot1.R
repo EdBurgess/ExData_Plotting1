0d66730d2d6c9635be46699584bcd7d8c695365b
Ed Burgess <ezedburgess@gmail.com>
2016-05-08 12:16:23 -0400

    Added files via upload

---
+++ plot1.R
@@ -0,0 +1,5 @@
+df=read.csv("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings="?")
+Date1<-subset(df,Date=="1/2/2007" | Date=="2/2/2007")
+hist(Date1$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.copy(png,file="plot1.png")
+dev.off()
